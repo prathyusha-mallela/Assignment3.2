a87a349c03ec2dda684ae8a70ca5d9ab184879d4
prathyusha-mallela <pratyushamv@gmail.com>
2017-12-26 22:49:01 +0530

    Add files via upload

---
+++ Assignment32.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 26 22:46:34 2017
+
+@author: Win 8.1
+"""
+
+#Assignment 3.2
+def separator(text):
+    return [letter for letter in text if letter!='']
+separator('ACADGILD')
